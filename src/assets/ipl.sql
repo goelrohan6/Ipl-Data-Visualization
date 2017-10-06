8ea12c96335905a30ee7101a34fd80dbf3bbc086
goelrohan6 <goelrohan6@gmail.com>
2017-10-06 22:31:58 +0530

    added some more json

--- src/assets/ipl.sql
+++ src/assets/ipl.sql
@@ -1,21 +1,29 @@
--- 1 Maximum Toss Winners (maximumTosses.json)
+--1 Maximum Toss Winners (maximumTosses.json)
 SELECT toss_winner, COUNT(*) FROM Matches GROUP BY toss_winner
 
--- 2 Toss decisions accross seasons (tossDecisions.json)
+--2 Toss decisions accross seasons (tossDecisions.json)
 SELECT season, toss_decision, COUNT(*) FROM Matches GROUP BY season, toss_decision
 
--- 3 Matches played accross each season (matchesPlayed.json)
+--3 Matches played accross each season (matchesPlayed.json)
 SELECT season, COUNT(*) FROM Matches GROUP BY season
 
--- 4 Total Runs accross seasons (totalRuns.json)
+--4 Total Runs accross seasons (totalRuns.json)
 SELECT M.season, SUM(D.total_runs) 
 FROM deliveries AS D, matches AS M WHERE D.match_id = M.id GROUP BY M.season
 
--- 5 Average runs accross seasons
+--5 Average runs accross seasons
 
--- 6 Total Sixes and Fours (sixesFours.json)
+
+--6 Total Sixes and Fours (sixesFours.json)
 SELECT * FROM SIX NATURAL JOIN FOURS
 
--- 7 Runs Per Over By Teams Across Seasons (runsOver.json)
+--7 Runs Per Over By Teams Across Seasons (runsOver.json)
 SELECT batting_team,over,SUM(total_runs) 
 FROM matches AS M,deliveries AS D WHERE M.id=D.match_id GROUP BY over, batting_team
+
+--8 Favorite Grounds (favGrounds.json)
+SELECT venue, count(*) as total from matches group by venue order by total desc
+
+--9 max Player of the matches (playerOfMatch.json)
+SELECT player_match, count(*) as total from matches group by player_match order by total desc
+
