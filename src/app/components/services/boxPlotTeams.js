037b3555148a36926af93cb4e8d01165072dcc5f
goelrohan6 <goelrohan6@gmail.com>
2017-10-07 01:22:43 +0530

    json configured for box plot nvd3

--- src/app/components/services/boxPlotTeams.js
+++ src/app/components/services/boxPlotTeams.js
@@ -6,112 +6,136 @@
             function() {
                 var obj = {};
                 obj.teams = [{
-                    "data": [{
-                            "batting_team": "CSK",
-                            "max": "246",
-                            "min": "55",
-                            "q1" : "141",
-                            "q2" : "160",
-                            "q3" : "180",
-                        },
-                        {
-                            "batting_team": "DC",
-                            "max": "214",
-                            "min": "82",
-                            "q1" : "137",
-                            "q2" : "151",
-                            "q3" : "169",
-                        },
-                        {
-                            "batting_team": "DD",
-                            "max": "231",
-                            "min": "56",
-                            "q1" : "132",
-                            "q2" : "150",
-                            "q3" : "167",
-                        },
-                        {
-                            "batting_team": "GL",
-                            "max": "196",
-                            "min": "104",
-                            "q1" : "131",
-                            "q2" : "158",
-                            "q3" : "164",
-                        },
-                        {
-                            "batting_team": "KXP",
-                            "max": "232",
-                            "min": "88",
-                            "q1" : "130",
-                            "q2" : "157",
-                            "q3" : "176",
-                        },
-                        {
-                            "batting_team": "KT",
-                            "max": "184",
-                            "min": "74",
-                            "q1" : "109",
-                            "q2" : "135",
-                            "q3" : "148",
-                        },
-                        {
-                            "batting_team": "KKR",
-                            "max": "222",
-                            "min": "61",
-                            "q1" : "132",
-                            "q2" : "150",
-                            "q3" : "165",
-                        },
-                        {
-                            "batting_team": "MI",
-                            "max": "218",
-                            "min": "68",
-                            "q1" : "141",
-                            "q2" : "159",
-                            "q3" : "174",
-                        },
-                        {
-                            "batting_team": "PW",
-                            "max": "192",
-                            "min": "99",
-                            "q1" : "118",
-                            "q2" : "139",
-                            "q3" : "156",
-                        },
-                        {
-                            "batting_team": "RR",
-                            "max": "223",
-                            "min": "58",
-                            "q1" : "132",
-                            "q2" : "147",
-                            "q3" : "170",
-                        },
-                        {
-                            "batting_team": "RP",
-                            "max": "195",
-                            "min": "76",
-                            "q1" : "103",
-                            "q2" : "159",
-                            "q3" : "166",
-                        },
-                        {
-                            "batting_team": "Royal Challengers Bangalore",
-                            "max": "263",
-                            "min": "2",
-                            "q1" : "135",
-                            "q2" : "157",
-                            "q3" : "176",
-                        },
-                        {
-                            "batting_team": "SH",
-                            "max": "208",
-                            "min": "44",
-                            "q1" : "132",
-                            "q2" : "146",
-                            "q3" : "163",
+                        label: "CSK",
+                        values: {
+                            whisker_high: 246,
+                            whisker_low: 55,
+                            Q1: 141,
+                            Q2: 160,
+                            Q3: 180
                         }
-                    ]
-                }]
+                    },
+                    {
+                        label: "DC",
+                        values: {
+                            whisker_high: 214,
+                            whisker_low: 82,
+                            Q1: 137,
+                            Q2: 151,
+                            Q3: 169
+                        }
+                    },
+                    {
+                        label: "DD",
+                        values: {
+                            whisker_high: 231,
+                            whisker_low: 56,
+                            Q1: 132,
+                            Q2: 150,
+                            Q3: 167
+                        }
+                    },
+                    {
+                        label: "GL",
+                        values: {
+                            whisker_high: 196,
+                            whisker_low: 104,
+                            Q1: 131,
+                            Q2: 158,
+                            Q3: 164
+                        }
+                    },
+                    {
+                        label: "KXP",
+                        values: {
+                            whisker_high: 232,
+                            whisker_low: 88,
+                            Q1: 130,
+                            Q2: 157,
+                            Q3: 176
+                        }
+                    },
+                    {
+                        label: "KT",
+                        values: {
+                            whisker_high: 184,
+                            whisker_low: 74,
+                            Q1: 109,
+                            Q2: 135,
+                            Q3: 148
+                        }
+                    },
+                    {
+                        label: "KKR",
+                        values: {
+                            whisker_high: 222,
+                            whisker_low: 61,
+                            Q1: 132,
+                            Q2: 150,
+                            Q3: 165
+                        }
+                    },
+                    {
+                        label: "MI",
+                        values: {
+                            whisker_high: 218,
+                            whisker_low: 68,
+                            Q1: 141,
+                            Q2: 159,
+                            Q3: 174
+                        }
+                    },
+                    {
+                        label: "PW",
+                        values: {
+                            whisker_high: 192,
+                            whisker_low: 99,
+                            Q1: 118,
+                            Q2: 139,
+                            Q3: 156
+                        }
+                    },
+                    {
+                        label: "RR",
+                        values: {
+                            whisker_high: 223,
+                            whisker_low: 58,
+                            Q1: 132,
+                            Q2: 147,
+                            Q3: 170
+                        }
+                    },
+                    {
+                        label: "RP",
+                        values: {
+                            whisker_high: 195,
+                            whisker_low: 76,
+                            Q1: 103,
+                            Q2: 159,
+                            Q3: 166
+                        }
+                    },
+                    {
+                        label: "RCB",
+                        values: {
+                            whisker_high: 263,
+                            whisker_low: 2,
+                            Q1: 135,
+                            Q2: 157,
+                            Q3: 176
+                        }
+                    },
+                    {
+                        label: "SH",
+                        values: {
+                            whisker_high: 208,
+                            whisker_low: 44,
+                            Q1: 132,
+                            Q2: 146,
+                            Q3: 163
+                        }
+                    }
+                ];
                 return obj;
             }
         ]);
